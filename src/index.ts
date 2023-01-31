05d054f9cac05388284b53286d4254ee5aeafbe7
Dinesh Tamang <dineshtamang7263@gmail.com>
2023-01-31 13:52:44 +0530

    Update

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,6 @@ export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
 export * from './middlewares/current-user';
+
+
+export * from './constants/auth/user.interfaces'
